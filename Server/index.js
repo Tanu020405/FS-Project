b86d0ff48b5d33b66b67d1201a0e26f182f8499c
Tanu020405 <tanishqsingh233001@gmail.com>
2025-10-23 00:23:30 +0530

    .

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import resourceRoutes from "./src/routes/resourceroutes.js";
 
 const app = express();
 app.use(express.json()); // JSON body parse karega
@@ -11,7 +12,7 @@ app.post("/data", (req, res) => {
   console.log(req.body); // ye Postman se aaya data print karega
   res.send("Data received successfully");
 });
-
+app.use("/api", resourceRoutes);
 app.listen(3000, () => {
   console.log("Server started on http://localhost:3000");
 });
